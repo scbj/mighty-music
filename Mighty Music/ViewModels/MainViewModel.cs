744e7e32f40d478ed662d02b03d484e0de8f405c
Sacha Bejaud <sacha.bejaud@ynov.com>
2016-02-28 15:30:03 +0100

    add "Mighty Music" project

---
+++ Mighty Music/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Mighty_Music.Views;
+using MVVM.Pattern.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mighty_Music.ViewModels
+{
+    public class MainViewModel : ViewModelBase, IViewModel<MainView>
+    {
+        // Properties
+        public MainView View { get; set; }
+        object IViewModel.View
+        {
+            get { return View; }
+            set { View = (MainView)value; }
+        }
+    }
+}
---
+++ Mighty Music/Views/MainView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Mighty_Music.ViewModels;
+using MVVM.Pattern.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mighty_Music.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour MainView.xaml
+    /// </summary>
+    public partial class MainView : Window, IView<MainViewModel>
+    {
+        // Properties
+        public MainViewModel ViewModel { get; set; }
+        object IView.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainViewModel)value; }
+        }
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
